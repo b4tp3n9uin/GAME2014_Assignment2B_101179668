8bd6ae30b08c7823f25f2907e0b3d00fb498678d
Matthew <MatthewMakepeace32@gmail.com>
2020-11-28 17:29:32 -0500

    Added Player Movement and Camera

---
+++ Assets/Scripts/PlayerControls/PlayerBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [Header("Controls")]
+    public Joystick joystick;
+    public float joystickHorizontalSensitivity;
+    public float joystickVerticalSensitivity;
+    public float horizontalForce;
+    public float verticleForce;
+    public bool isGrounded;
+
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (isGrounded == true)
+        {
+            if (joystick.Horizontal > joystickHorizontalSensitivity)
+            {
+                // move right
+                rigidBody.AddForce(Vector3.right * horizontalForce * Time.deltaTime);
+                spriteRenderer.flipX = false;
+                animator.SetInteger("AnimState", 1);
+            }
+            else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+            {
+                // move left
+                rigidBody.AddForce(Vector3.left * horizontalForce * Time.deltaTime);
+                spriteRenderer.flipX = true;
+                animator.SetInteger("AnimState", 1);
+            }
+            else if (joystick.Vertical > joystickVerticalSensitivity)
+            {
+                // jump
+                rigidBody.AddForce(Vector3.up * verticleForce * Time.deltaTime);
+                animator.SetInteger("AnimState", 2);
+            }
+            else
+            {
+                // idle
+                animator.SetInteger("AnimState", 0);
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        isGrounded = true;
+
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        isGrounded = false;
+    }
+}
